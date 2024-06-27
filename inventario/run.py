3df0336a7c75b29c7fa4618a1c1455c038e6ddfb
RoW <shnaida408@gmail.com>
2024-06-26 19:44:19 -0600

    Construyendo el entorno principal

--- inventario/run.py
+++ inventario/run.py
@@ -7,6 +7,14 @@ inventario = Flask(__name__)
 def home():
     return "Primer Inventario , Hola"
 
+@inventario.route('/usuarios')
+def home():
+    return "Ventana de Usuarios ----  RO"
+
+@inventario.route('/actualiza')
+def home():
+    return "Ventana de Actualizar ----- RO"
+
 inventario.run(debug=True, port=4500)
 
 
