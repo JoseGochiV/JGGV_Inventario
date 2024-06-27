76a17d86b34dbbb0cf5423fe2e23a86ac33a0dfa
joseG <jgochivergara@yahoo.com.mx>
2024-06-26 19:59:47 -0600

    Nuevo Inventario RO

--- inventario/run.py
+++ inventario/run.py
@@ -5,15 +5,19 @@ inventario = Flask(__name__)
 
 @inventario.route('/')
 def home():
-    return "Primer Inventario , Hola"
+    return "Primer Inventario , Hola:Compadre"
 
 @inventario.route('/usuarios')
-def home():
+def User():
     return "Ventana de Usuarios ----  RO"
 
 @inventario.route('/actualiza')
-def home():
+def Actualiza():
     return "Ventana de Actualizar ----- RO"
+
+@inventario.route('/Borrar')
+def Borrar():
+    return "Ventana de Borrar ----- Lupillo"
 
 inventario.run(debug=True, port=4500)
 
