79f0065f8491181b1b0161ffba39255170e4ab08
joseG <jgochivergara@yahoo.com.mx>
2024-07-13 20:52:32 -0600

    Nuevo Inventario version 2.0

--- inventario/run.py
+++ inventario/run.py
@@ -1,23 +1,34 @@
 from flask import Flask
+from flask_mysqldb import MySQL, MySQLdb
 
 inventario = Flask(__name__)
+
+mysql = MySQL(inventario)
+
+inventario.config['MYSQL_HOST']='127.0.0.1'
+inventario.config['MYSQL_USER'] = "JoseG"
+inventario.config['MYSQL_PASSWORD'] = "121290"
+inventario.config['MYSQL_DB'] = "inventario_v1.0"
 
 
 @inventario.route('/')
 def home():
+    
     return "Primer Inventario , Hola:Compadre"
 
 @inventario.route('/usuarios')
 def User():
-    return "Ventana de Usuarios ----  RO"
+    return "Ventana de Usuarios ----  RO:2"
 
 @inventario.route('/actualiza')
 def Actualiza():
-    return "Ventana de Actualizar ----- RO"
+    return "Ventana de Actualizar ----- RO:2"
 
 @inventario.route('/Borrar')
 def Borrar():
-    return "Ventana de Borrar ----- Lupillo"
+    return "Ventana de Borrar ----- Lupillo:2"
+
+
 
 inventario.run(debug=True, port=4500)
 
